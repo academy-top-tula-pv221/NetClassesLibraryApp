22b243dc7e588396a1aa30cf589824b373a52d79
maxefimov <efimov.academy.top@gmail.com>
2023-02-13 21:23:19 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace ClassLibrary
+{
+    public class Unit
+    {
+        string name;
+        public Unit(string name)
+        {
+            this.name = name;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Unit name {name}");
+        }
+    }
+}
---
+++ NetClassesLibraryApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary;
+
+namespace NetClassesLibraryApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Unit unit = new("Archer");
+            unit.Print();
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
